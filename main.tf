b7f42143873d9483f44230b8fe5e7247a0b49080
root <root@localhost.localdomain>
2019-09-24 07:40:43 -0400

    ISTIO

--- main.tf
+++ main.tf
@@ -433,9 +433,9 @@ kubectl config use-context ${var.cluster_name} --kubeconfig=${local_file.kubecon
 kubectl apply -f create-helm-service-account.yml --kubeconfig=${local_file.kubeconfig.filename}
 helm init --service-account helm --upgrade --wait --kubeconfig=${local_file.kubeconfig.filename}
 helm install -n spin stable/spinnaker --namespace spinnaker -f ${local_file.spinnaker_chart.filename} --timeout 600 --version 1.8.1 --wait --kubeconfig=${local_file.kubeconfig.filename}
-helm install istio.io/istio-init --name istio-init --namespace istio-system --kubeconfig=${local_file.kubeconfig.filename} --timeout 600
-helm install istio.io/istio --name istio --namespace istio-system --no-crd-hook --kubeconfig=${local_file.kubeconfig.filename}
+helm install istio.io/istio-init --name istio-init --namespace istio-system --kubeconfig=${local_file.kubeconfig.filename}
 bash forward_spin_gate.sh
+helm install istio.io/istio --name istio --namespace istio-system --wait  --kubeconfig=${local_file.kubeconfig.filename}
 LOCAL_EXEC
   }
   depends_on = ["google_container_node_pool.primary","local_file.kubeconfig","kubernetes_namespace.spinnaker","local_file.spinnaker_chart","google_storage_bucket_iam_binding.spinnaker-bucket-iam","google_pubsub_subscription_iam_binding.spinnaker_pubsub_iam_read","local_file.spinnaker_install_sh"]
