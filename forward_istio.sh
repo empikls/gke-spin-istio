6280b166ce60b8311a34f30b5f0a3a4b4824be2a
RebelUkraine <support4uf1@gmail.com>
2019-09-26 18:04:35 +0300

    Update forward_istio.sh

--- forward_istio.sh
+++ forward_istio.sh
@@ -4,4 +4,6 @@ GRAF_POD=$(kubectl -n istio-system get pod -l app=grafana --kubeconfig=kubeconfi
 kubectl -n istio-system port-forward ${GRAF_POD} 3000 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
 PROM_POD=$(kubectl -n istio-system get pod -l app=prometheus --kubeconfig=kubeconfig -ojsonpath='{.items[0].metadata.name}')
 kubectl -n istio-system port-forward ${PROM_POD} 9090 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
+ZIP_POD=$(kubectl -n istio-system get pod -l app=jaeger -ojsonpath='{.items[0].metadata.name}')
+kubectl -n istio-system port-forward ${ZIP_POD} 9411 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
 
